74b24ae2e02acf4e93a5eee90f32c9d46c11ff5f
dchristofolli <danielchristofolli@gmail.com>
2020-01-25 17:21:38 -0300

    postgres bombando

--- impl/src/main/java/com/dchristofolli/poc/v1/repository/UserEntity.java
+++ impl/src/main/java/com/dchristofolli/poc/v1/repository/UserEntity.java
@@ -3,27 +3,33 @@ package com.dchristofolli.poc.v1.repository;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @lombok.Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Document(collection = "Users")
+@Entity(name = "Users")
 public class UserEntity {
     @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id;
 
-    @Indexed(unique = true)
+    @Column(columnDefinition = "text", unique = true)
     private String name;
 
-    @Indexed(unique = true)
+    @Column(columnDefinition = "text", unique = true)
     private String cpf;
 
-    @Indexed(unique = true)
+    @Column(columnDefinition = "text", unique = true)
     private String email;
 
+    @Column(columnDefinition = "text", unique = true)
     private String password;
 }
--- impl/src/main/java/com/dchristofolli/poc/v1/repository/UserRepository.java
+++ impl/src/main/java/com/dchristofolli/poc/v1/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.dchristofolli.poc.v1.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends MongoRepository<UserEntity, String> {
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 
     Optional<UserEntity> findById(String id);
 
