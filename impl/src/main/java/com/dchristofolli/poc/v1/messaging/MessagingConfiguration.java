65d50b397403b550ef5379bc0077920327ef2db5
Daniel Christofolli <daniel.christofolli@southsystem.com.br>
2020-09-10 16:24:34 -0300

    criando request findAll

--- impl/src/main/java/com/dchristofolli/poc/v1/messaging/MessagingConfiguration.java
+++ impl/src/main/java/com/dchristofolli/poc/v1/messaging/MessagingConfiguration.java
@@ -1,9 +1,6 @@
 package com.dchristofolli.poc.v1.messaging;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -15,6 +12,9 @@ import org.springframework.context.annotation.Configuration;
 public class MessagingConfiguration {
     @Value("${rabbitmq.exchangeName}")
     private String exchangeName;
+
+    @Value("${rabbitmq.listAllExchange}")
+    private String listAllUsersExchange;
 
     @Value("${rabbitmq.specificQueueName}")
     private String specificQueueName;
@@ -28,6 +28,11 @@ public class MessagingConfiguration {
     @Bean
     public TopicExchange appExchange() {
         return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public DirectExchange directExchange(){
+        return new DirectExchange(listAllUsersExchange);
     }
 
     @Bean
--- impl/src/main/java/com/dchristofolli/poc/v1/messaging/Sender.java
+++ impl/src/main/java/com/dchristofolli/poc/v1/messaging/Sender.java
@@ -3,8 +3,10 @@ package com.dchristofolli.poc.v1.messaging;
 import com.dchristofolli.poc.v1.repository.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,12 +21,30 @@ public class Sender {
     @Value("${rabbitmq.routingKey}")
     private final String routingKey;
 
+    @Value("${rabbitmq.findAllRoutingKey}")
+    private String findAllRoutingKey;
+
+    private final DirectExchange directExchange;
+
     public UserEntity send(UserEntity userEntity) {
-        log.info("Sending message...");
+        log.info("Sending entity...");
         rabbitTemplate.convertAndSend(
             exchange,
             routingKey,
             userEntity);
         return userEntity;
+    }
+
+    public void sendRequestToFindAll() {
+        UserEntity userEntity = UserEntity
+            .builder()
+            .build();
+        rabbitTemplate.convertSendAndReceiveAsType(
+            directExchange.getName(),
+            findAllRoutingKey,
+            userEntity,
+            new ParameterizedTypeReference<>() {
+            }
+        );
     }
 }
